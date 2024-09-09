b0130273162f632500c45a93af3758a63db66e1f
AlexToader10 <alextoader2002@gmail.com>
2024-09-09 15:25:20 +0300

    pid?!

--- smekerez.py
+++ smekerez.py
@@ -1,5 +1,23 @@
 import pygame
 from sys import exit
+import random
+import time
+
+class PID:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.previous_error = 0
+        self.integral = 0
+
+    def compute(self, setpoint, measured_value):
+        error = setpoint - measured_value
+        self.integral += error
+        derivative = error - self.previous_error
+        self.previous_error = error
+        return self.kp * error + self.ki * self.integral + self.kd * derivative
+
 
 class Robot:
     def __init__(self, x, y, width, height, color):
@@ -29,20 +47,37 @@ class Robot:
         self.x += x
         self.y += y
         
-    def rotate(self, angle):
-        self.angle += angle  # Update the angle
+    def _rotate(self, angle):
+        self.angle += min(angle, 0.5) # are si el o viteza maxima cum zic..
+        
+    def rotate(self, gyro, target_angle):
+        pid = PID(0.1, 0, 0.05)
+        current_angle = gyro.get_angle()
+        control_signal = pid.compute(target_angle, current_angle)
+        self._rotate(control_signal)
+        return current_angle
 
+class Gyro:
+    def __init__(self, robot: Robot):
+        self.robot = robot
+        self.precision = 5.0
+        
+    def get_angle(self):
+        return self.robot.angle + random.uniform(-self.precision, self.precision)
+    
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
 
     teren = pygame.image.load("./teren/vinyle_table_2024_FINAL_V1.png")
+    clock = pygame.time.Clock()
 
     # Define the color black
     black = (0, 0, 0)
 
     robot = Robot(90, 90, 120, 120, black)
+    gyro = Gyro(robot)
 
     while True:
         for event in pygame.event.get():
@@ -52,7 +87,10 @@ def main():
                 
         screen.blit(teren, (0, 0))
         robot.draw(screen)
-        robot.rotate(1)  # Rotate the robot by 1 degree
+        
+        robot.rotate(gyro, 90)
+        clock.tick(60)
+        
         
         
         # Draw a black rectangle
